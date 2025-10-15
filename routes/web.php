54800ee5994604d35e4f3aa2cd164af7149a7fa0
indahriskipanjaitan <riskiindah071@gmail.com>
2025-10-15 13:18:03 +0700

    Menggunakan Bootstrap

--- routes/web.php
+++ routes/web.php
@@ -2,14 +2,19 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/home', function () {
+Route::get('/', function () {
     return view('home');
 });
 
-Route:: get('/about', function () {
+Route::get('/about', function () {
     return view('about');
 });
 
-Route:: get('/profile', function () {
-    return view('profile');
+Route::get('/mahasiswa', function () {
+    return view('mahasiswa');
+});
+
+Route::get('/profile', function () {
+    $nama = 'Indah';
+    return view('profile', compact('nama'));
 });
